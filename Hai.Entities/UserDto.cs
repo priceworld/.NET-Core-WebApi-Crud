2226960979c4480febaf3fd8ef7f68dd6cfb80b0
priceworld <mactavish6060@gmail.com>
2022-11-22 11:09:18 +0300

    userDto key

--- Hai.Entities/UserDto.cs
+++ Hai.Entities/UserDto.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Hai.Entities
 {
     public class UserDto
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
         public int? UserTypeId { get; set; }
+        [StringLength(100)]
         public string UserName { get; set; }
+        [StringLength(50)]
         public string Password { get; set; }
         public DateTime ValidationDate { get; set; }
         public DateTime? LastLoginDate { get; set; }
